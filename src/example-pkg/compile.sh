420d1b03e589fa0c5e72a032eae05c7453c7bb7f
mamccollum <mamccollum06@gmail.com>
2023-04-24 02:51:01 -0500

    Clean up sources

--- src/example-pkg/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd src
-cc -o example example.c
-cd ..
-mkdir -p root/usr/bin/
-cp src/example root/usr/bin/example
-
-
--- src/example-pkg/src/example.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("This is an example program!!! :)");
-	return 0;
-}
